23033687cef3a1d070dbf7a30d6e4600386282bc
Vijayant Joshi <jobs.vijayantjoshi@gmail.com>
2025-06-30 12:16:51 +0530

    Changed the netlify server to render sever and added .env file

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "https://cosmic-lokum-59861f.netlify.app/.netlify/functions/index/api",
+    baseURL: `${apiUrl}`,
     headers: {
         "Content-Type": "application/json",
     },
